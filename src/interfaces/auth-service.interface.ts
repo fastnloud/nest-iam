4dcd448159fc26b22d7c7c12b9b72956843bce75
jaap <jaap.vanturnhout@gmail.com>
2023-09-08 19:15:22 +0200

    Changed order of methods in interface

--- src/interfaces/auth-service.interface.ts
+++ src/interfaces/auth-service.interface.ts
@@ -2,13 +2,13 @@ import { IToken } from './token.interface';
 import { IUser } from './user.interface';
 
 export interface IAuthService {
+  getUserOrFail(id: string): Promise<IUser>;
   getValidTokenOrFail(
     id: string,
     type: string,
     requestId?: string,
   ): Promise<IToken>;
   getValidUserOrFail(username: string): Promise<IUser>;
-  getUserOrFail(id: string): Promise<IUser>;
   removeToken(id: string): Promise<void>;
   saveToken(token: IToken): Promise<void>;
 }
